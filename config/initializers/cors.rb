d628ac1448eb8d0c9ff74002a5598e978ee47b8a
iSaacSigei <kiplangatisack.is@gmail.com>
2024-08-21 09:47:28 +1000

    cookies

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,12 +2,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://womall.vercel.app' # Replace with your frontend domain
+    origins 'https://womall.vercel.app'  # Your frontend domain
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       expose: ['Authorization'],
+      credentials: true,  # Allows cookies to be sent
       max_age: 600
   end
 end
