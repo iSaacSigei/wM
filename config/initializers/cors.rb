83eb2442ba5a2fa809e624f67dda095e0483fee3
iSaacSigei <kiplangatisack.is@gmail.com>
2024-08-21 10:13:24 +1000

    Fix CORS configuration to specify allowed origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,14 @@
 # config/initializers/cors.rb
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # Temporarily allow all origins for debugging
+    origins 'https://womall.vercel.app'  # Your frontend domain
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       expose: ['Authorization'],
-      credentials: true,
+      credentials: true,  # Allows cookies to be sent
       max_age: 600
   end
 end
